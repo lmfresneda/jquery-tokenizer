8a78abcfdae8393e7d86f98cc383ca128f6e18b2
lmfresneda <miguelfr.reg@gmail.com>
2015-09-06 12:36:21 +0200

    Mejora en implementación de funciones privadas

--- jquery-tokenizer-0.1.js
+++ jquery-tokenizer-0.1.js
@@ -18,7 +18,8 @@
         var KEY_CODE_ENTER = 13,
             KEY_CODE_SUPR = 46,
             KEY_CODE_DEL = 8;
-        var uniqueArray = function (array) {
+        //PRIVATE METHODS
+        var _uniqueArray = function (array) {
             var a = array.concat();
             for (var i = 0; i < a.length; ++i) {
                 for (var j = i + 1; j < a.length; ++j) {
@@ -28,31 +29,21 @@
             }
             return a;
         }
-        if (!String.__format) {
-            String.__format = function () {
-                var s = arguments[0];
-                for (var i = 0; i < arguments.length - 1; i++) {
-                    var reg = new RegExp("\\{" + i + "\\}", "gm");
-                    s = s.replace(reg, arguments[i + 1]);
-                }
-                return s;
-            };
-        }
-        var getWrapperValues = function ($input) {
+        var _getWrapperValues = function ($input) {
             var $wrapperValues = $("<div></div>")
                             .addClass("tokenizer-values")
                             .width($input.width() - 7);
             return $wrapperValues;
         }
-        var removeAllToken = function () {
+        var _removeAllToken = function () {
             $(".tokenizer-token").remove();
         }
-        var removeToken = function($tokens) {
+        var _removeToken = function($tokens) {
             $tokens.each(function () {
                 $(this).remove();
             });
         }
-        var getToken = function (value) {
+        var _getToken = function (value) {
             var $token = $("<span></span>")
                             .addClass("tokenizer-token")
                             .text(value)
@@ -64,38 +55,89 @@
                 $(this).toggleClass("tokenizer-token-active");
             });
             $token.find("span").on("click", function () {
-                removeToken($token);
+                _removeToken($token);
             });
             return $token;  
         }
-        var showMaxAll = function ($wrapperValues) {
-            var $token = getToken($.fn.tokenizer.defaults.lang.max_all);
+        var _showMaxAll = function ($wrapperValues) {
+            var $token = _getToken($.fn.tokenizer.defaults.text.max_all);
             $token.addClass("tokenizer-token-max");
             $token.removeAttr("data-value");
             $wrapperValues.append($token);
             setTimeout(function() {
-                removeToken($token);
+                _removeToken($token);
             }, 1500)
         }
-        var getValues = function() {
+        var _getValues = function() {
             var tokens = [];
             $(".tokenizer-token[data-value]").each(function () {
                 tokens.push($(this).attr("data-value"));
             });
             return tokens;
         }
+        var _buildRegExp = function(separators) {
+            var strReg = "\s*[";
+            for (var i = 0; i < separators.length; i++) {
+                strReg += separators[i];
+            }
+            strReg += "]\s*";
+            return new RegExp(strReg);
+        }
+        var _printTokens = function (tokens, $wrapperValues) {
+            _removeAllToken();
+            $.each(tokens, function (k, el) {
+                el = $.trim(el);
+                $wrapperValues.append(_getToken(el));
+            });
+        }
+        var _operations = {
+            get: function (args) {
+                if (args.length > 1) {
+                    throw new Error("Número de argumentos inválidos, se requiere ('option') solamente");
+                }
+                return _getValues();
+            },
+            set: function (args) {
+                var str;
+                if (args.length != 2) {
+                    throw new Error("Número de argumentos inválidos, se requiere ('option', String|Array)");
+                }
+                if (typeof args[1] === "string") {
+                    //only input
+                    str = args[1];
+                } else if (args[1] instanceof Array) {
+                    //array input
+                    str = args[1].join($.fn.tokenizer.defaults.separators[0]);
+                } else {
+                    throw new Error("Segundo parámetro inválido, se requiere ('option', String|Array)");
+                }
+                $this.val(str);
+                $this.trigger($.Event("keypress", { keyCode: KEY_CODE_ENTER }));
+                return $this;
+            },
+            del: function (args) {
+                if (args.length != 2) {
+                    throw new Error("Número de argumentos inválidos, se requiere ('option', String)");
+                }
+                if (typeof args[1] === "string") {
+                    $(".tokenizer-token[data-value='" + args[1] + "']").remove();
+                } else {
+                    throw new Error("Segundo parámetro inválido, se requiere ('option', String)");
+                }
+                return $this;
+            }
+        }
 
         if (!arguments.length || $.isPlainObject(arguments[0])) {
-            //bind
+            //INIT BINDING
             var settings = !arguments.length ? {} : arguments[0];
             $(document).on("keydown", function (e) {
                 if (e.keyCode == KEY_CODE_SUPR || e.keyCode == KEY_CODE_DEL) {
-                    removeToken($(".tokenizer-token-active"));
+                    _removeToken($(".tokenizer-token-active"));
                 }
             });
 
             settings = $.extend({}, $.fn.tokenizer.defaults, settings);
-            
 
             return $this.each(function () {
                 var $input = $(this);
@@ -104,19 +146,15 @@
                                 .addClass("tokenizer-wrapper")
                                 .width($input.width());
                 $input.wrap($wrapper);
-                var $wrapperValues = getWrapperValues($input);
+                var $wrapperValues = _getWrapperValues($input);
                 $input.parent().append($wrapperValues);
                 $input.css("margin-bottom", "0px");
+
                 $input.off().on("keypress", function (e) {
                     if (e.keyCode == KEY_CODE_ENTER) {
                         e.preventDefault();
                         if ($.trim($input.val()) != "") {
-                            var strReg = "\s*[";
-                            for (var i = 0; i < settings.characters.length; i++) {
-                                strReg += settings.characters[i];
-                            }
-                            strReg += "]\s*";
-                            var reg = new RegExp(strReg);
+                            var reg = _buildRegExp(settings.separators);
                             var tokens = $input.val().split(reg);
 
                             //controlamos max_input
@@ -124,7 +162,7 @@
                                 tokens = tokens.splice(0, settings.max_input);
                             }
 
-                            var tokens = getValues().concat(tokens);
+                            var tokens = _getValues().concat(tokens);
                             var showMax = false;
                             //controlamos max_all
                             if (settings.max_all > 0) {
@@ -136,77 +174,48 @@
                             }
 
                             if (!settings.repeat) {
-                                tokens = uniqueArray(tokens);
+                                tokens = _uniqueArray(tokens);
                             }
 
-                            removeAllToken();
-                            $.each(tokens, function (k, el) {
-                                el = $.trim(el);
-                                $wrapperValues.append(getToken(el));
-                            });
+                            _printTokens(tokens, $wrapperValues);
+
                             if (showMax)
-                                showMaxAll($wrapperValues);
+                                _showMaxAll($wrapperValues);
                         }
                         $input.val("");
                     }
                 });
             });
         } else {
-
+            //ANOTHER REQUEST
             switch(arguments[0]) {
                 case "get":
                 case "val":
                     //get value
-                    if (arguments.length > 1) {
-                        throw new Error("Número de argumentos inválidos, se requiere ('option') solamente");
-                    }
-                    return getValues();
+                    return _operations.get(arguments);
                 case "set":
                 case "input":
                     //set value
-                    if (arguments.length != 2) {
-                        throw new Error("Número de argumentos inválidos, se requiere ('option', String|Array)");
-                    }
-                    if (typeof arguments[1] === "string") {
-                        //only input
-                        $this.val(arguments[1]);
-                        $this.trigger($.Event("keypress", { keyCode: KEY_CODE_ENTER }));
-                    } else if (arguments[1] instanceof Array) {
-                        //array input
-                        var str = arguments[1].join($.fn.tokenizer.defaults.characters[0]);
-                        $this.val(str);
-                        $this.trigger($.Event("keypress", { keyCode: KEY_CODE_ENTER }));
-                    } else {
-                        throw new Error("Segundo parámetro inválido, se requiere ('option', String|Array)");
-                    }
-                    return $this;
+                    return _operations.set(arguments);
                 case "del":
                 case "delete":
                 case "rm":
                 case "remove":
                     //remove value
-                    if (arguments.length != 2) {
-                        throw new Error("Número de argumentos inválidos, se requiere ('option', String)");
-                    }
-                    if (typeof arguments[1] === "string") {
-                        $(".tokenizer-token[data-value='" + arguments[1] + "']").remove();
-                    } else {
-                        throw new Error("Segundo parámetro inválido, se requiere ('option', String)");
-                    }
-                    return $this;
+                    return _operations.del(arguments);
                 default:
-                    throw new Error(String.__format("Opción '{0}' no válida", arguments[0]));
+                    throw new Error("Opción '"+ arguments[0] +"' no válida");
             }
-
         }
     };
 
+    //SETTINGS
     $.fn.tokenizer.defaults = $.fn.tk.defaults = {
-        characters: [",", ";"],
+        separators: [",", ";"],
         repeat: false,
         max_all: 0,
         max_input: 0,
-        lang: {
+        text: {
             max_all: "Máximo alcanzado"
         }
     };
